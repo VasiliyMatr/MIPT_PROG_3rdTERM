54a85c71b1f22854665ff9357c3d44f09e4c9e41
VasiliyMatr <vasiliymatrenin@gmail.com>
2021-09-07 21:41:19 +0300

    added message for empty input

--- SEMINAR_TASKS/T00_BIZZBUZZ/bizz-buzz.c
+++ SEMINAR_TASKS/T00_BIZZBUZZ/bizz-buzz.c
@@ -19,6 +19,12 @@ enum argProp_t parseArg( const char * arg );
 
 int main( int argc, char** argv )
 {
+    if (argc == 1)
+        fprintf (stderr,
+                 "Usage:" "\n"
+                 "%s <ARG> [ARGS]" "\n",
+                 argv [0]);
+
     for (size_t argId = 1; argId < argc; argId++)
     {
         enum argProp_t argProp = parseArg (argv [argId]);
@@ -43,8 +49,8 @@ int main( int argc, char** argv )
 
             default:
                 fprintf (stderr,
-                    "ERROR WITH argProp_t VALUE: FILE=%s LINE=%d VALUE=%d" "\n",
-                    __FILE__, __LINE__, argProp);
+                         "ERROR WITH argProp_t VALUE: FILE=%s LINE=%d VALUE=%d" "\n",
+                         __FILE__, __LINE__, argProp);
                 return -1;
         }
     }
